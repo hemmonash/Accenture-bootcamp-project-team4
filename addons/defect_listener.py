64e428a15e5f2b726825954bd36189d1a79c4e32
hemmonash <hem.malhotra@monash.edu>
2019-07-18 10:33:25 +1000

    FEATURE: Email template configured and added data pulling

--- addons/defect_listener.py
+++ addons/defect_listener.py
@@ -1,19 +1,61 @@
 from elasticsearch import Elasticsearch
 import requests
 from flask import Flask, render_template, request
-from sendEmail import *
+import json
+import sendEmail
+# from sendEmail import *
 
 client = Elasticsearch()
 
+query_total_defects = {
+    "query":{
+   "bool":{
+     "should": {
+         "match": {
+           "fields.issuetype.name": "Bug"
+         }
+       },
+       "must" : {
+         "bool" :{
+           "must_not" : [
+             {"match": { "fields.status.statusCategory.name": "Done"}},
+             {"match": {"fields.status.statusCategory.name": "Published"}},
+             {"match": {"fields.status.statusCategory.name": "Approved"}},
+             {"match": {"fields.status.statusCategory.name": "Cancelled"}},
+             {"match": {"fields.status.statusCategory.name": "Rejected"}},
+             {"match": {"fields.status.statusCategory.name": "Purchased"}},
+             {"match": {"fields.status.statusCategory.name": "Accepted"}},
+             {"match": {"fields.status.statusCategory.name": "lost"}},
+             {"match": {"fields.status.statusCategory.name": "won"}}
+             ]
+        }
+     }
+   }
+ }
+}
+
+res = client.search(index="quality_management_bootcamp", body=query_total_defects)
+print("Got %d Hits" % len(res['hits']['hits']))
+
+open_defects = []
+for every_hit in res['hits']['hits']:
+    result = every_hit['_source']
+    open_defects.append(result)
+
+for every in open_defects:
+    print(every['fields']['status']['statusCategory']['name'])
+print(open_defects)
+
+
 # read the total number of defects and if the threshhold exceeds the user entered value, send email
-total_defects = client.search(index="total_defect_index")
+total_defects = len(open_defects)
 
 # read the most up-to-date user entered threshold number
-user_entered_defects = client.search(index="alert_index")
+user_entered_defects_std_query = client.search(index="alert_index_std")
+user_entered_defects_std = int(user_entered_defects_std_query['hits']['hits'][0]['_source']['user_threshold'])
+print(user_entered_defects_std)
 
-# run query to get the open defects 
-
-
-if (total_defects > user_entered_defects):
+if (total_defects > user_entered_defects_std):
     # call email function and send email to scrum master
-    sendMailToScrumMaster()
+    print("Alter triggered, email will be sent!")
+    sendEmail.sendMailToScrumMaster(open_defects)
--- addons/sendEmail.py
+++ addons/sendEmail.py
@@ -2,15 +2,79 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 import smtplib
 
+
 def sendMailToScrumMaster(defect_objs):
     """
     defect_objs: param for the defect objects
     """
     # create message object instance
     msg = MIMEMultipart()
+    message = '''
+    <!DOCTYPE html>
+    <html lang="en">
 
-    message = ""
+    <head>
+        <title>Alert config page</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
 
+        <style>
+            /* Set height of the grid so .sidenav can be 100% (adjust as needed) */
+            .row.content {
+                height: 550px
+            }
+
+            /* Set gray background color and 100% height */
+            .sidenav {
+                background-color: #f1f1f1;
+                height: 100%;
+            }
+
+            /* On small screens, set height to 'auto' for the grid */
+            @media screen and (max-width: 767px) {
+                .row.content {
+                    height: auto;
+                }
+            }
+        </style>
+    </head>
+
+    <body>
+        <center>
+        <table class="table">
+        <thead>
+            <tr>
+            <th scope="col">#</th>
+            <th scope="col">First</th>
+            <th scope="col">Last</th>
+            <th scope="col">Handle</th>
+            </tr>
+        </thead>
+        <tbody>
+    '''
+    row_index = 1
+    for every_defect in defect_objs:
+        # assigne name, last up
+        defect_id = int(every_defect['key'])
+        assignee_name = every_defect['fields']['assignee']['name']
+        last_updated = every_defect['fields']['updated']
+        project_name = every_defect['fields']['project']['name']
+        project_summary = every_defect['fields']['summary']
+
+        message += ('<tr><th scope="row">%d</th><td>%d</td><td>%s</td><td>%s</td><td>%s</td><td>%s</td></tr>', row_index, defect_id, project_name, assignee_name, last_updated, project_summary)
+        row_index += 1
+
+    message += '''
+        </tbody>
+        </table>
+        </center>
+    </body>
+    </html>
+    '''
     # setup the parameters of the message
     password = "bootcamp1234"
     msg['From'] = "accenturebootcampteam4@gmail.com"
