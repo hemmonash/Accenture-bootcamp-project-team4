97ed23c00f122d611987cba3515c3b02e51086b7
hemmonash <hem.malhotra@monash.edu>
2019-07-18 11:25:29 +1000

    TEST

--- addons/sendEmail.py
+++ addons/sendEmail.py
@@ -14,29 +14,102 @@ def sendMailToScrumMaster(defect_obj):
     msg['To'] = "kdas0001@student.monash.edu" #"asfiya.memon@accenture.com" #["febinaly@getnada.com", "aditivenkateshr@gmail.com", "asfiya.memon@accenture.com", "krishnendu.c.das@accenture.com", "chethzz@hotmail.com", "peter.liang.official@gmail.com", "riababyc@gmail.com"]
     msg['Subject'] = "Defect threshold exceeded!"
 
-    # Create the body of the message (a plain-text and an HTML version).
-    text = "Hi!\nHow are you?\nHere is the link you wanted:\nhttp://www.python.org"
-    html = """\
-    <html>
-    <head></head>
+    message = '''
+    <!DOCTYPE html>
+    <html lang="en">
+
+    <head>
+        
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+
+        <style>
+            /* Set height of the grid so .sidenav can be 100% (adjust as needed) */
+            .row.content {
+                height: 550px
+            }
+
+            /* Set gray background color and 100% height */
+            .sidenav {
+                background-color: #f1f1f1;
+                height: 100%;
+            }
+
+            /* On small screens, set height to 'auto' for the grid */
+            @media screen and (max-width: 767px) {
+                .row.content {
+                    height: auto;
+                }
+            }
+        </style>
+    </head>
+
     <body>
-        <p>Hi!<br>
-        How are you?<br>
-        Here is the <a href="http://www.python.org">link</a> you wanted.
+        <center>
+        <div class="jumbotron" style="margin: 5%;">
+        <h1 class="display-3">Alerts Successfully Configured!</h1>
+        <p class="lead">You will recieve emails consisting of the defect details if the threshold is crossed over a week and also if the defects pass the threshold before certain days prior to sprint end.</p>
+        <hr class="my-4">
+        <p>You can always change these details <a href="https://techbootcamp.mywizard360.com/melbootcamp2019julyt4_5000">here</a></p>
+        <p class="lead">
+          
+        <table class="table">
+        <thead>
+            <tr>
+            <th scope="col">Defect #</th>
+            <th scope="col">Defect ID</th>
+            <th scope="col">Project Name</th>
+            <th scope="col">Assignee Name</th>
+            <th scope="col">Last Updated</th>
+            <th scope="col">Project Summary</th>
+            </tr>
+        </thead>
+        <tbody>
+    '''
+    print(defect_obj)
+    row_index = 1
+    for every_defect in defect_obj:
+        # assigne name, last up
+        defect_id = every_defect['fields']['project']['key']
+        assignee_name = every_defect['fields']['assignee']['name']
+        last_updated = every_defect['fields']['updated']
+        project_name = every_defect['fields']['project']['name']
+        project_summary = every_defect['fields']['summary']
+
+        table_row_entry = '<tr><th scope="row">{}</th><td>{}</td><td>{}</td><td>{}</td><td>{}</td><td>{}</td></tr>'.format(row_index, defect_id, project_name, assignee_name, last_updated, project_summary)
+        print(table_row_entry)
+        message += table_row_entry
+        row_index += 1
+
+    message += '''
+        </tbody>
+        </table>
+        <br>
+        <a class="btn btn-primary btn-lg" href="https://techbootcamp.mywizard360.com/melbootcamp2019julyt4_5601/app/kibana#/dashboard/9c7ca7e0-a861-11e9-8eee-e33d10f51134?_g=(filters%3A!()%2CrefreshInterval%3A(pause%3A!t%2Cvalue%3A0)%2Ctime%3A(from%3A'2016-09-28T15%3A05%3A52.732Z'%2Cto%3A'2019-07-31T14%3A00%3A00.000Z'))" role="button">Go to Dashboard!</a>
         </p>
+        </div>
+        </center>
     </body>
     </html>
-    """
+    '''
+
+    # Create the body of the message (a plain-text and an HTML version).
+    text = "Hi!\nHow are you?\nHere is the link you wanted:\nhttp://www.python.org"
+    
 
     # Record the MIME types of both parts - text/plain and text/html.
     part1 = MIMEText(text, 'plain')
-    part2 = MIMEText(html, 'html')
+    message_renderer = MIMEText(message, 'html')
 
     # Attach parts into message container.
     # According to RFC 2046, the last part of a multipart message, in this case
     # the HTML message, is best and preferred.
     msg.attach(part1)
-    msg.attach(part2)
+    msg.attach(message_renderer)
 
     # Send the message via local SMTP server.
     s = smtplib.SMTP('smtp.gmail.com: 587')
